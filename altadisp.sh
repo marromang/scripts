e3a003cb921c0cd79071169bce83f7bd57a2d860
Maria Romero <m.romeroangulo@gmail.com>
2017-11-30 09:20:18 +0100

    ultimo cambio definitivo

--- altadisp.sh
+++ altadisp.sh
@@ -22,12 +22,15 @@ ssh-add /home/maria/.ssh/openstack
 
 
 #reglas de iptables en anfitrion:
+iptables -F
 iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 22 -j ACCEPT
 iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 80 -j ACCEPT
+ip r del 192.168.122.0/24 dev virbr0 proto kernel scope link src 192.168.122.1
 
 #comprobar conexion a ssh
 echo 'Prueba de conexion:'
 iptables -L
+sleep 15
 msg=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.94 echo 'conectado')
 if [[ $msg == 'conectado' ]];
 then
@@ -37,11 +40,11 @@ then
 		echo 'Comprobando...'
 
 		memoria=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.94 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
-		if [[ $memoria -le '10000' ]];
+		if [[ $memoria -le '100' ]];
 		then
 			echo 'La memoria de la maquina 1 está llena. Se va a cambiar a la máquina 2.'
 			#desasociamos el volumen 
-			ssh -i home/maria/.ssh/openstack root@192.168.122.94 umount /mnt
+			ssh -i /home/maria/.ssh/openstack root@192.168.122.94 umount /mnt
 			virsh -c qemu:///system detach-disk mv1 /dev/logVol/mv1
 			#se apaga la maquina
 			virsh shutdown mv1
@@ -68,7 +71,7 @@ then
 			virsh -c qemu:///system attach-disk mv2 /dev/logVol/mv1 vdb
 
 			#montar para que los datos de la web vayan a /var/www/html/
-			ssh -i /home/maria/.ssh/openstack root@192.168.122.93 mount /dev/vdb /var/
+			ssh -i /home/maria/.ssh/openstack root@192.168.122.93 mount /dev/vdb /var/www/
 
 			echo 'Cambio realizado.'
 			flagmv1='error'
@@ -76,24 +79,21 @@ then
 	done
 
 	while [[ $flagmv2 == 'correcto' ]]; do
+		echo 'Comprobando...'
 		#se comprueba la memoria
 		mem=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.93 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
-		if [[ $mem -le '10000' ]];
+		if [[ $mem -le '100' ]];
 		then
 			echo 'La memoria de la maquina 2 está llena. Se va a redimensionar.'
 
 			#redimension en vivo
 			virsh setmem mv2 --size 2G --live
-
+			echo 'redimension hecha'
 			#memoria redimensionada
 			flagmv2='error'
 		fi
 	done
-	echo 'Informacion de mv1: '
-	virsh dominfo mv1
-	echo 'Informacion de mv2: '
-	virsh dominfo mv2
-
+	echo 'FIN'
 else 	
 	echo 'No se ha podido conectar con la máquina.'
 fi
