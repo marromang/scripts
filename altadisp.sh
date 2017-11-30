f7e9643d52397f6f38128ceb029b6fa18a9a7b2e
Maria Romero <m.romeroangulo@gmail.com>
2017-11-30 19:30:59 +0100

    Update altadisp.sh

--- altadisp.sh
+++ altadisp.sh
@@ -5,6 +5,9 @@ ip_mv2='192.168.122.93'
 flagmv1='correcto'
 flagmv2='correcto'
 
+unset SSH_AUTH_SOCK
+unset KRB5CCNAME
+ç
 echo 'Configurando la red...'
 ip l add br0 type bridge
 ip a add 192.168.122.3/24 dev br0
@@ -14,24 +17,19 @@ ip l set br0 up
 #levantar mv1
 virsh start mv1
 
-echo 'Configuración de acceso por ssh: '
-
-
-eval "$(ssh-agent -s)"
-ssh-add /home/maria/.ssh/openstack
-
-
 #reglas de iptables en anfitrion:
-iptables -F
-iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 22 -j ACCEPT
-iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 80 -j ACCEPT
+/sbin/iptables -F
+/sbin/iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -I FORWARD -d 192.168.122.94 -p tcp --dport 80 -j ACCEPT
 ip r del 192.168.122.0/24 dev virbr0 proto kernel scope link src 192.168.122.1
 
 #comprobar conexion a ssh
 echo 'Prueba de conexion:'
-iptables -L
+/sbin/iptables -L
 sleep 15
-msg=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.94 echo 'conectado')
+msg=$(/usr/bin/ssh -i ~/cron-rsa root@192.168.122.94 echo 'conectado')
+echo $msg
+
 if [[ $msg == 'conectado' ]];
 then
 	echo 'Prueba realizada con exito.'
@@ -39,22 +37,22 @@ then
 		#se comprueba la memoria
 		echo 'Comprobando...'
 
-		memoria=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.94 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
+		memoria=$(/usr/bin/ssh -i ~/cron-rsa root@192.168.122.94 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
 		if [[ $memoria -le '100' ]];
 		then
 			echo 'La memoria de la maquina 1 está llena. Se va a cambiar a la máquina 2.'
 			#desasociamos el volumen 
-			ssh -i /home/maria/.ssh/openstack root@192.168.122.94 umount /mnt
+			/usr/bin/ssh -i ~/cron-rsa root@192.168.122.94 umount /mnt
 			virsh -c qemu:///system detach-disk mv1 /dev/logVol/mv1
 			#se apaga la maquina
 			virsh shutdown mv1
 
 			#se redimensiona el volumen
 			
-			lvextend -L +1G /dev/logVol/mv1
-			mount /dev/logVol/mv1 /mnt
-			xfs_growfs /dev/logVol/mv1
-			umount /mnt
+			/sbin/lvextend -L +1G /dev/logVol/mv1
+			/bin/mount /dev/logVol/mv1 /mnt
+			/sbin/xfs_growfs /dev/logVol/mv1
+			/bin/umount /mnt
 
 			#se incia la maquina 2
 			echo 'Iniciando mv2'
@@ -63,15 +61,15 @@ then
 
 			#cambio de reglas de iptables
 			echo 'Reglas de iptables'
-			iptables -F
-			iptables -I FORWARD -d 192.168.122.93 -p tcp --dport 22 -j ACCEPT
-			iptables -I FORWARD -d 192.168.122.93 -p tcp --dport 80 -j ACCEPT
+			/sbin/iptables -F
+			/sbin/iptables -I FORWARD -d 192.168.122.93 -p tcp --dport 22 -j ACCEPT
+			/sbin/iptables -I FORWARD -d 192.168.122.93 -p tcp --dport 80 -j ACCEPT
 
 			#asociar el nuevo volumen
 			virsh -c qemu:///system attach-disk mv2 /dev/logVol/mv1 vdb
 
 			#montar para que los datos de la web vayan a /var/www/html/
-			ssh -i /home/maria/.ssh/openstack root@192.168.122.93 mount /dev/vdb /var/www/
+			/usr/bin/ssh -i ~/cron-rsa root@192.168.122.93 mount /dev/vdb /var/www/
 
 			echo 'Cambio realizado.'
 			flagmv1='error'
@@ -81,7 +79,7 @@ then
 	while [[ $flagmv2 == 'correcto' ]]; do
 		echo 'Comprobando...'
 		#se comprueba la memoria
-		mem=$(ssh -i /home/maria/.ssh/openstack root@192.168.122.93 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
+		mem=$(/usr/bin/ssh -i ~/cron-rsa root@192.168.122.93 free -m | grep 'Mem' | tr -s " " ";"| cut -d ";" -f 4)
 		if [[ $mem -le '100' ]];
 		then
 			echo 'La memoria de la maquina 2 está llena. Se va a redimensionar.'
