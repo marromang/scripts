5c696b4b9c19adc334ddbaa13326fd2babf16b00
Maria Romero <m.romeroangulo@gmail.com>
2018-01-09 13:16:42 +0100

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -3,7 +3,7 @@
 backup_user='maria.romero'
 backup_description='Incremental'
 
-for (( i = 0; i < 4; i++ )); do
+for (( i = 1; i < 4; i++ )); do
 	case $i in
 		1)
 			host='mickey'
@@ -20,9 +20,6 @@ for (( i = 0; i < 4; i++ )); do
 	esac
 	#datos de la copia
 	datos=$( mariadb -u root -e "select Job, Level, JobStatus, RealEndTime from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and Name='$host' group by Name;" )
-	#level = F / I
-	#jobStatus = T / f
-	#RealEndTime =  2018-01-05 23:55:04
 	
 	backup_label=$( echo $datos | cut -d " " -f 5 )
 	
@@ -31,7 +28,6 @@ for (( i = 0; i < 4; i++ )); do
 		backup_description='Full'
 	fi
 	
-
 	backup_JobStatus=$( echo $datos | cut -d " " -f 7 ) # T or f
 	if [[ $backup_JobStatus == 'T' ]]; then
 		backup_status='200'
@@ -39,9 +35,10 @@ for (( i = 0; i < 4; i++ )); do
 	else
 		backup_status='400'
 		backup_mode='Failed'
+		echo "Fallo en la copia del host '$host' con fecha '$backup_date"  | mail -s "Error en Bacula" m.romeroangulo@gmail.com
 	fi
 
 	backup_date=$( echo $datos | cut -d " " -f 8 )
 	
-	"psql -h 172.22.200.110 -U maria.romero -d db_backup -c insert into backups values('$backup_user', '$backup_host', '$backup_label', '$backup_description', '$backup_status', '$backup_date', '$backup_mode' );"
+	psql -h 172.22.200.110 -U maria.romero -d db_backup -c "insert into backups values('$backup_user', '$backup_host', '$backup_label', '$backup_description', '$backup_status', '$backup_date', '$backup_mode' );"
 done 
