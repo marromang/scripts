21777e912477ef435ab78d8f1b8386c3133d207a
Maria Romero <m.romeroangulo@gmail.com>
2018-01-09 11:52:43 +0100

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #variables
 backup_user='maria.romero'
-
-#cadenas de conexion
-connMysql='mariadb -u root'
-connPsql='psql -h 172.22.200.110 -U maria.romero -d db_backup'
+backup_description='Incremental'
 
 for (( i = 0; i < 4; i++ )); do
 	case $i in
@@ -23,7 +20,6 @@ for (( i = 0; i < 4; i++ )); do
 	esac
 	#datos de la copia
 	datos=$( mariadb -u root -e "select Job, Level, JobStatus, RealEndTime from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and Name='$host' group by Name;" )
-	echo $datos
 	#level = F / I
 	#jobStatus = T / f
 	#RealEndTime =  2018-01-05 23:55:04
@@ -31,14 +27,21 @@ for (( i = 0; i < 4; i++ )); do
 	backup_label=$( echo $datos | cut -d " " -f 5 )
 	
 	backup_Level=$( echo $datos | cut -d " " -f 6 ) # F or I
-	backup_description= Depende de backupType #full or incremental
+	if [[ $backup_level == 'F' ]]; then
+		backup_description='Full'
+	fi
 	
+
 	backup_JobStatus=$( echo $datos | cut -d " " -f 7 ) # T or f
-	backup_status= depende de backup_JobStatus
-	backup_mode= depende de status #auto or failed, if failed there should be a manual copy
+	if [[ $backup_JobStatus == 'T' ]]; then
+		backup_status='200'
+		backup_mode='Automatic'
+	else
+		backup_status='400'
+		backup_mode='Failed'
+	fi
 
 	backup_date=$( echo $datos | cut -d " " -f 8 )
 	
-
-	#connPsql -c 'insert into backups values($backup_user, $backup_host, $backup_label,$backup_description, $backup_status, $backup_date, $backup_mode);'
+	"psql -h 172.22.200.110 -U maria.romero -d db_backup -c insert into backups values('$backup_user', '$backup_host', '$backup_label', '$backup_description', '$backup_status', '$backup_date', '$backup_mode' );"
 done 
