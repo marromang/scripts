a80f92c183f44ee8621580da2820c478f8992bfd
Maria Romero <m.romeroangulo@gmail.com>
2018-01-10 09:52:17 +0100

    final

--- backups.sh
+++ backups.sh
@@ -1,9 +1,10 @@
-
 #!/bin/bash
-#constantes
+
+#constant values
 backup_user='maria.romero'
 backup_description='Incremental'
 backup_mode='Automatica'
+backup_node_dir="172.22.200.110"
 
 for (( i = 1; i < 4; i++ )); do
         case $i in
@@ -20,26 +21,40 @@ for (( i = 1; i < 4; i++ )); do
                         backup_host='172.22.200.56'
                         ;;
         esac
-        #datos de la copia
-        datos=$( mariadb -u root -e "select Job, Level, JobStatus, RealEndTime from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and Name='$host' group by Name;" )
+        
+        #check connection to host
+        if [[ "fping $backup_host" ]]; then
+                datos=$( mariadb -u root -e "select Job, Level, JobStatus, RealEndTime from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and Name='$host' group by Name;" )
 
-        backup_label=$( echo $datos | cut -d " " -f 5 )
+                backup_label=$( echo $datos | cut -d " " -f 5 )
 
-        backup_Level=$( echo $datos | cut -d " " -f 6 ) # F or I
-        if [[ $backup_level == 'F' ]]; then
-                backup_description='Full'
-        fi
+                backup_Level=$( echo $datos | cut -d " " -f 6 ) # F or I
+                if [[ $backup_level == 'F' ]]; then
+                        backup_description='Full'
+                fi
 
-        backup_JobStatus=$( echo $datos | cut -d " " -f 7 ) # T or f
-        if [[ $backup_JobStatus == 'T' ]]; then
-                backup_status='200'
+                backup_JobStatus=$( echo $datos | cut -d " " -f 7 ) # T or f
+                if [[ $backup_JobStatus == 'T' ]]; then
+                        backup_status='200'
+                else
+                        backup_status='400'
+                        echo "$host backup on $backup_date failed."  | mail -s "Bacula backup error" m.romeroangulo@gmail.com
+                fi
+
+                backup_date=$( echo $datos | cut -d " " -f 8 )
+
         else
-                backup_status='400'
-                echo "Fallo en la copia del host $host con fecha $backup_date"  | mail -s "Error en Bacula" m.romeroangulo@gmail.com
+                backup_status='100'
+                echo "$host backup on $backup_date failed. No connection to host."  | mail -s "Bacula host error" m.romeroangulo@gmail.com
+
         fi
 
-        backup_date=$( echo $datos | cut -d " " -f 8 )
-
-        psql -h 172.22.200.110 -U maria.romero -d db_backup -c "insert into backups values('$backup_user', '$backup_host','$backup_label','$backup_description', '$backup_status', '$backup_date', '$backup_mode' );"
+        #check connection to db
+        if [[ !("fping $backup_node_dir") ]]; then
+                backup_status='500'
+                echo "$host backup on $backup_date failed. No connection to db."  | mail -s "Bacula db error" m.romeroangulo@gmail.com
+        else    
+                psql -h $backup_node_dir -U maria.romero -d db_backup -c "insert into backups values('$backup_user', '$backup_host','$backup_label','$backup_description', '$backup_status', '$backup_date', '$backup_mode' );"
+        fi
+       
 done 
- 
