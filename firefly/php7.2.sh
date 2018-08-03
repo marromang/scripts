be7fb005410017a9b33f42914675a081fcc36578
Maria Romero <m.romeroangulo@gmail.com>
2018-08-03 13:32:43 +0200

    Delete php7.2.sh

--- firefly/php7.2.sh
+++
@@ -1,6 +0,0 @@
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php7.2-fpm
-sudo apt-get install -y php7.2
-sudo apt-get -y install curl php7.2-pgsql php7.2-curl php7.2-json php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-imagick php7.2-redis php7.2-zip
-sudo systemctl disable apache2
